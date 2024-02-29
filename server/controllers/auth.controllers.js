7760605fa5dade6264ae71e499374dd963500122
EISENHOWER <chepkosum@gmail.com>
2024-02-29 20:05:19 +0300

    complete signup functionality

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -1,17 +1,21 @@
-import User from '../models/user.models.js'
-import bcryptjs from 'bcryptjs'
-import { errorHandler } from '../utils/error.js';
-
+import User from '../models/user.models.js';
+import bcryptjs from 'bcryptjs';
 
 export const signup = async (req, res, next) => {
-        const { username, email, password } = req.body;
-        const hashedpassword = bcryptjs.hashSync(password, 10);
-        const newUser = new User({ username, email, password: hashedpassword });
-        try {
-                await newUser.save()
-                res.status(201).json('User created successfully')
-        } catch (error) {
-                next(error)
-        }
+    const { username, email, password } = req.body;
 
-}
+    try {
+        // Hash the password asynchronously to avoid any blocking issues
+        const saltRounds = 10;
+        const hashedPassword = await bcryptjs.hash(password, saltRounds);
+
+        const newUser = new User({ username, email, password: hashedPassword });
+
+        await newUser.save();
+        
+        res.status(201).json('User created successfully');
+    } catch (error) {
+        // Handle errors, log them, and pass them to the next middleware
+        next(error);
+    }
+};
