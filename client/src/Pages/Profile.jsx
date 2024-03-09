50ac3f53693720c603a52b39ebac06989e3255a6
EISENHOWER <chepkosum@gmail.com>
2024-03-09 15:35:33 +0300

    complete delete listing functionality

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -18,6 +18,7 @@ const Profile = () => {
   const [updateSuccess, setUpdateSuccess] = useState(false)
   const [showListingError, setShowListingError] = useState(false)
   const [userListings, setUserListings] = useState([])
+  const [listingDeleteError, setListingDeleteError] = useState(false)
   const dispatch = useDispatch();
 
 
@@ -132,6 +133,23 @@ const handleShowListing = async () =>{
   }
 }
 
+ 
+const handleListingDelete = async (listingId) =>{
+  try {
+    const res = await fetch(`/api/listing/delete/${listingId}`,{
+      method: 'DELETE',
+    });
+    const data = await res.json();
+    if(data.success === false){
+      setListingDeleteError(true);
+    }
+    
+    setUserListings((prev)=>prev.filter((listing)=>listing._id !==listingId));
+  } catch (error) {
+    setListingDeleteError(true)
+  }
+}
+ 
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -236,7 +254,7 @@ const handleShowListing = async () =>{
             <p >{listing.name}</p>
              </Link>
              <div className=" flex flex-col items-center">
-              <button className='text-red-700'>Delete</button>
+              <button onClick={()=>handleListingDelete(listing._id)} className='text-red-700'>Delete</button>
               <button className='text-green-700'>Edit</button>
              </div>
         </div>
