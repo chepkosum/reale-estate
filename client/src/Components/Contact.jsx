42646c528ac73f46b4f5b235a00686c7e2334bf4
EISENHOWER <chepkosum@gmail.com>
2024-03-11 12:27:22 +0300

    add contact landlord functionality to the listing page.

--- client/src/Components/Contact.jsx
+++ client/src/Components/Contact.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import {Link} from 'react-router-dom'
 
-const Contact = () => {
+const Contact = ({ listing }) => {
+  const [landlord, setLandlord] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [message, setMessage] = useState('')
+
+  const handleChange = (e)=>{
+    setMessage(e.target.value);
+  }
+
+  useEffect(() => {
+    const fetchLandlord = async () => {
+        try {
+          const res = await fetch(`/api/user/${listing.userRef}`);
+          
+          // Check if the response status is OK before attempting to parse JSON
+          if (!res.ok) {
+            throw new Error(`Error: ${res.status} ${res.statusText}`);
+          }
+      
+          const data = await res.json();
+          setLandlord(data);
+          setLoading(false);
+        } catch (error) {
+          console.error("Error fetching landlord data:", error);
+          setError("Error fetching landlord data. Please try again later.");
+          setLoading(false);
+        }
+      };
+      
+
+    fetchLandlord();
+  }, [listing.userRef]);
+
   return (
-    <div>Contact</div>
-  )
-}
+    <>
+      {loading && <p>Loading...</p>}
+      {error && <p className="text-red-700">Error: {error}</p>}
+      {landlord && (
+        <div className="flex flex-col gap-2">
+          <p>Contact <span className="font-semibold">{landlord.username}</span> for:  
+          <span className="font-semibold">{listing.name.toLowerCase()}</span></p>
+          <textarea onChange={handleChange}  name="message" id="message" value={message} rows="2"></textarea>
+          
+          <Link to={`mailto:${landlord.email}?subject=Regarding
+           ${listing.name}&body=${message}`}
+           className="bg-slate-700 text-white text-center
+           p-3 uppercase rounded-lg hover:opacity-95">
+            Send Message
+          </Link>
+        </div>
+      )}
+    </>
+  );
+};
 
-export default Contact
+export default Contact;
--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -3,21 +3,21 @@ import bcryptjs from 'bcryptjs'
 import { errorHandler } from '../utils/error.js';
 import Listing from "../models/listing.models.js";
 
-export const test = (req, res)=>{
+export const test = (req, res) => {
     res.json({
-        message : 'Hello world edwin , youre the best',
+        message: 'Hello world edwin , youre the best',
     });
 };
 
-export const updateUser = async (req, res,next)=>{
-    if(req.user.id !==req.params.id)
-     return next(errorHandler(401, "You can only update your own account"));
+export const updateUser = async (req, res, next) => {
+    if (req.user.id !== req.params.id)
+        return next(errorHandler(401, "You can only update your own account"));
     try {
-        if(req.body.password) {
+        if (req.body.password) {
             req.body.password = bcryptjs.hashSync(req.body.password, 10)
         }
 
-        const updateUser = await User.findByIdAndUpdate(req.params.id,{
+        const updateUser = await User.findByIdAndUpdate(req.params.id, {
             $set: {
                 username: req.body.username,
                 email: req.body.email,
@@ -25,10 +25,10 @@ export const updateUser = async (req, res,next)=>{
                 avatar: req.body.avatar,
             }
 
-        }, {new: true});
+        }, { new: true });
 
         const { password, ...rest } = updateUser._doc;
-        
+
         res.status(200).json(rest)
     } catch (error) {
         next(error)
@@ -36,30 +36,48 @@ export const updateUser = async (req, res,next)=>{
 }
 
 export const deleteUser = async (req, res, next) => {
-    if(req.user.id !== req.params.id)
-    return next(errorHandler(401, 'You can only delete your own account'));
-try {
-    await User.findByIdAndDelete(req.params.id);
-    res.clearCookie('access_token');
-    res.status(200).json('User has been deleted!');
-} catch (error) {
-    next(error)
-}
+    if (req.user.id !== req.params.id)
+        return next(errorHandler(401, 'You can only delete your own account'));
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.clearCookie('access_token');
+        res.status(200).json('User has been deleted!');
+    } catch (error) {
+        next(error)
+    }
 }
 
 
 //Listing all user listings
 
-export const getUserListings = async (req,res,next) =>{
-    if (req.user.id == req.params.id){
+export const getUserListings = async (req, res, next) => {
+    if (req.user.id == req.params.id) {
         try {
-            const listings = await Listing.find({userRef: req.params.id});
+            const listings = await Listing.find({ userRef: req.params.id });
             res.status(200).json(listings);
-            
+
         } catch (error) {
             next(error)
         }
-    } else{
+    } else {
         return next(errorHandler(401, "You can only view your own listings"))
     }
+}
+
+//Getting a user route
+
+export const getUser = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.params.id);
+
+        if (!user) {
+            return next(errorHandler(404, 'User not found'))
+        }
+        const { password: pass, ...rest } = user._doc;
+        
+        res.status(200).json(rest);
+    } catch (error) {
+        next(error)
+    }
+
 }
