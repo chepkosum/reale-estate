8a11c0c822f55774e0b3d8ed49ee6b4b2a6300b3
EISENHOWER <chepkosum@gmail.com>
2024-03-16 10:12:27 +0300

    updated listing item and listing controller

--- client/src/Components/ListingItem.jsx
+++ client/src/Components/ListingItem.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
-import {MdLocationOn} from 'react-icons/md'
+import { MdLocationOn } from 'react-icons/md'
 
 const ListingItem = ({listing}) => {
   return (
--- server/controllers/listing.controllers.js
+++ server/controllers/listing.controllers.js
@@ -84,23 +84,23 @@ export const getListing = async (req,res,next) =>{
 
 export const getListings = async (req,res,next) =>{
     try {
-        const limit = parseInt(req.query.limit) || 6;
+        const limit = parseInt(req.query.limit) || 9;
         const startIndex = parseInt(req.query.startIndex) || 0;
         let offer = req.query.offer;
 
-        if(offer===undefined || offer===false){
+        if(offer === undefined || offer==='false'){
             offer={$in:[false, true]};
         }
 
         let furnished = req.query.furnished;
 
-        if(furnished===undefined || furnished===false){
+        if(furnished===undefined || furnished==='false'){
             furnished={$in: [false, true]};
         }
 
         let parking = req.query.parking;
 
-        if(parking===undefined || parking===false){
+        if(parking===undefined || parking==='false'){
             parking={$in: [false, true]};
         }
 
@@ -117,7 +117,7 @@ export const getListings = async (req,res,next) =>{
         const order = req.query.order || 'desc';
 
         const listings = await Listing.find({
-            name: {$regex: searchTerm, $options: 'i'},
+            name: { $regex: searchTerm, $options: 'i'},
             offer,
             furnished,
             parking,
@@ -127,9 +127,6 @@ export const getListings = async (req,res,next) =>{
         }).limit(limit).skip(startIndex);
 
         return res.status(200).json(listings);
-
-
-
     } catch (error) {
         next(error)
     }
