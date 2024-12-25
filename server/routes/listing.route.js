4f8d27332c3d8159db7aed24718e7c128e268f6e
EISENHOWER <70235379+chepkosum@users.noreply.github.com>
2024-12-25 21:26:47 +0300

    Update listing.route.js

--- server/routes/listing.route.js
+++ server/routes/listing.route.js
@@ -1,4 +1,3 @@
-
 import express, { Router } from 'express'
 import { createListing, deleteListing, getListing, getListings, updateListing } from '../controllers/listing.controllers.js'
 import { verifyToken } from '../utils/verifyUser.js';
